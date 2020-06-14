d8541fee60451001d6d977c7015e0d0fb1639dd7
Imran Kheraj <ikheraj17@gmail.com>
2020-06-13 23:25:11 -0500

    Todo List items successfully adding on submit, text clearing properly after submission

--- App.js
+++ App.js
@@ -3,18 +3,38 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Todo from './components/todo'
 
 export default function App() {
+
   const [todos, setTodos] = useState([
     { text: "Learn about React Native" },
     { text: "Learn to use Hooks" },
     { text: "Meet Judy for ice cream" }
   ]);
+
   const [text, setText] = useState("");
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const clearText = () => {
+    setText("");
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!text) return;
+    addTodo(text);
+    clearText();
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>REACT NATIVE TODO LIST APP</Text>
       <TextInput 
         style={{height: 40, backgroundColor: 'white', margin: 30}}
-        defaultValue={text}
+        value={text}
+        onSubmitEditing={handleSubmit}
         onChangeText={text => setText(text)}
         placeholder={"Add a todo item here"}
         />
